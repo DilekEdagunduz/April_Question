08cd06084829c1700db931117344dee34c7cd5de
asus <asus@DESKTOP-7AA3IH2>
2022-04-13 11:21:59 +0300

    Sorular

---
+++ AprilQuestion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AprilQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1) 2010 ile 2022 arasında mesai saatleri (9:00-18:00) 1000 adet Random tarih oluştur ve bir tarih listesine ata
+
+            // 2) Bu listede hafta sonu olmasaın
+            // 3) Bu listedeki içerisnde tarihlerin kaçı subat ayı içerisndedir.
+
+
+            //7) Kullanıcı ekrandan bir yıl girsin ve o yıla ait tüm tarihleri ekrana yaz
+
+
+            //8) Kullanıcı ekrandan önce yıl sonra ay girsin ve bu ay ve yıla ait tüm tarihler sıralansın.
+
+
+            //9) 2010 SONRASI tarihleri ekrana yazdır.
+
+
+            //10) 2010-2015 arasında SADECE OCAK ayında geçen tarihleri ekrana yazdır.
+
+
+
+
+
+
+            // Şubat ayı sayısı için private tanımlanan bir nesne oluşturuldu.
+        } 
+    }
+}
+    
+
